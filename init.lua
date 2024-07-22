9265b8aebe4efddea326018850ed5228c8365e39
73anthonyL <73anthony.lu@gmail.com>
2024-07-22 10:00:00 -0700

    save this stuff before i migrate to lazy

--- init.lua
+++ init.lua
@@ -3,52 +3,59 @@ vim.g.mapleader = ','
 local packer = require('packer')
 local use = packer.use
 
--- Start Packer
+-- Start Papkee
 return packer.startup(function()
-    -- Packer can manage itself as a plugin
+    -- Packer
     use 'wbthomason/packer.nvim'
-
-    -- Add your other plugins here
+    -- Solarized colorscheme
     use 'shaunsingh/solarized.nvim'
+
+    -- Catpuccin colorschemes
     use { "catppuccin/nvim", as = "catppuccin" }
-
+    
+    -- Discord Presence 
     use 'andweeb/presence.nvim'
+    
+    -- File Explorer
+    use 'nvim-tree/nvim-tree.lua'
 
+    -- CompetiTest for Competitive Programming
     use {
         'xeluxee/competitest.nvim',
         requires = 'MunifTanjim/nui.nvim',
         config = function() require('competitest').setup() end
     }
-
+    
+    -- Lsp 
     use 'neovim/nvim-lspconfig'
+    
+    -- Status bar
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    
+    -- Code completion and its dependencies
     use 'hrsh7th/cmp-nvim-lsp'
     use 'hrsh7th/cmp-buffer'
     use 'hrsh7th/cmp-path'
     use 'hrsh7th/cmp-cmdline'
     use 'hrsh7th/nvim-cmp'
     use 'hrsh7th/cmp-nvim-lsp-signature-help'
-
     use 'hrsh7th/cmp-vsnip'
     use {
         'nvim-treesitter/nvim-treesitter',
         run = ':TSUpdate'
     }
-    use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.8',
-    -- or                            , branch = '0.1.x'
-      requires = { {'nvim-lua/plenary.nvim'} }
-    }
 
-    -- Add more plugins as needed
-    -- Example configuration for a plugin
-    -- You can configure plugins here using the `config` option
-    -- Example:
-    -- use {
-    --   'author/plugin-name',
-    --   config = function()
-    --     -- Configuration for the plugin
-    --   end
-    -- }
+    -- Fuzzy find and grep
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
+    
+    -- Comments
+    use 'tpope/vim-commentary'
 
 	-- General editor settings
     vim.opt.laststatus = 0
@@ -65,20 +72,23 @@ return packer.startup(function()
 	vim.opt.cinoptions:append("L0")
 	vim.cmd("syntax on")
 	vim.cmd("filetype indent on")
-	vim.cmd("filetype off")
+	vim.cmd("filetype on")
+    vim.cmd("filetype plugin on")
 	vim.opt_local.indentkeys:remove(":")
 	vim.opt.belloff = "all"
 	vim.opt.clipboard:append("unnamedplus")
+
+    -- Colorscheme setups
     vim.cmd.colorscheme "catppuccin-mocha"
-    --vim.cmd.colorscheme "solarized"
+	--require('solarized').set()
+
 	-- Keybindings for { completion, "jk" for escape, ctrl-a to select all
 	vim.api.nvim_set_keymap("i", "{<CR>", "{<CR>}<Esc>O", { noremap = true })
 	vim.api.nvim_set_keymap("i", "{}", "{}", { noremap = true })
 	vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = true })
 	vim.api.nvim_set_keymap("n", "<C-a>", "<esc>ggVG<CR>", { noremap = true })
 
-	-- Plugin-specific configuration
-	--require('solarized').set()
+    -- CompetiTest setup, with the compile flags and filenames
 	require('competitest').setup {
         compile_command = {
             cpp = {
@@ -104,48 +114,41 @@ return packer.startup(function()
         received_contests_problems_path = "$(JAVA_TASK_CLASS)/$(JAVA_TASK_CLASS).$(FEXT)",
 		received_problems_path = "$(CWD)/$(JAVA_TASK_CLASS)/$(JAVA_TASK_CLASS).$(FEXT)",
     }
-
+    
+    -- Setup Discord Rich Presence
     require("presence").setup({})
+
+    -- Set up the status bar
+    require('lualine').setup {
+        options = {
+            theme = 'ayu'
+        }
+    }
+    
+    -- File Explorer setup
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
+
+    -- optionally enable 24-bit colour
+    vim.opt.termguicolors = true
+
+    -- empty setup using defaults
+    require("nvim-tree").setup({
+        view = {
+            width = 45
+        },
+    })
+
+    -- Treesitter setup
     require'nvim-treesitter.configs'.setup {
-      -- A list of parser names, or "all" (the listed parsers MUST always be installed)
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cpp" },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      -- List of parsers to ignore installing (or "all")
-      --ignore_install = { "javascript" },
-
-      ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-      -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
-
-      highlight = {
-        enable = true,
-
-        -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-        -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-        -- the name of the parser)
-        -- list of language that will be disabled
-        --disable = { "c", "rust" },
-        -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-        --disable = function(lang, buf)
-            --local max_filesize = 100 * 1024 -- 100 KB
-            --local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-            --if ok and stats and stats.size > max_filesize then
-                --return true
-            --end
-        --end,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
+        ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "cpp" },
+        sync_install = false,
+        auto_install = true,
+        highlight = {
+            enable = true,
+            additional_vim_regex_highlighting = false,
+        },
     }
 
     -- Set up nvim-cmp.
@@ -154,47 +157,28 @@ return packer.startup(function()
     cmp.setup({
         snippet = {
           -- REQUIRED - you must specify a snippet engine
-          expand = function(args)
+            expand = function(args)
             vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-            -- vim.snippet.expand(args.body) -- For native neovim snippets (Neovim v0.10+)
-          end,
+            end,
         },
         window = {
           -- completion = cmp.config.window.bordered(),
           -- documentation = cmp.config.window.bordered(),
         },
         mapping = cmp.mapping.preset.insert({
-          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.abort(),
-          ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+            ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-e>'] = cmp.mapping.abort(),
+            ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' },
-          { name = 'vsnip' }, -- For vsnip users.
-          -- { name = 'luasnip' }, -- For luasnip users.
-          -- { name = 'ultisnips' }, -- For ultisnips users.
-          -- { name = 'snippy' }, -- For snippy users.
+            { name = 'nvim_lsp' },
+            { name = 'vsnip' }, -- For vsnip users.
         }, {
-          { name = 'buffer' },
+            { name = 'buffer' },
         })
     })
-
-    -- To use git you need to install the plugin petertriho/cmp-git and uncomment lines below
-    -- Set configuration for specific filetype.
-    --[[ cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-      { name = 'git' },
-    }, {
-      { name = 'buffer' },
-    })
-    })
-    require("cmp_git").setup() ]]-- 
-
     -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
     cmp.setup.cmdline({ '/', '?' }, {
         mapping = cmp.mapping.preset.cmdline(),
@@ -222,7 +206,8 @@ return packer.startup(function()
             debounce_text_changes = 150,
         }
     }
-
+    
+    -- Keybinds for compiling, running, 
     vim.api.nvim_command("set makeprg=g++\\ -Wall\\ -Wextra\\ -Wshadow\\ -Wconversion\\ -Wfloat-equal\\ -Wduplicated-cond\\ -Wlogical-op\\ -std=c++17\\ -o\\ '%:r'\\ '%'")
 	vim.api.nvim_set_keymap('n', '<leader>c', ':w <bar> make<CR>', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>d', ':w <bar> make<CR>', { noremap = true, silent = true })
@@ -235,7 +220,7 @@ return packer.startup(function()
 --  vim.api.nvim_set_keymap('n', '<leader>full', ':term ./%:r<CR>i', { noremap = true, silent = true })
     vim.api.nvim_set_keymap('n', '<leader>full', ":term ./'%:r'<CR>i", { noremap = true, silent = true })
 
-	vim.api.nvim_set_keymap('n', '<leader>r', ':CompetiTest receive problem<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>qr', ':CompetiTest receive problem<CR>', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>t', ':CompetiTest receive contest<CR>', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>add', ':CompetiTest add_testcase<CR>i', { noremap = true, silent = true })
 	vim.api.nvim_set_keymap('n', '<leader>edit', ':CompetiTest edit_testcase<CR>i', { noremap = true, silent = true })
@@ -246,4 +231,9 @@ return packer.startup(function()
     vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     --vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
     --vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    --
+
+	vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>f', ':NvimTreeFocus<CR>', { noremap = true, silent = true })
+	vim.api.nvim_set_keymap('n', '<leader>q', ':wqa<CR>', { noremap = true, silent = true })
 end)
